f557bd7bdb66f20925288025d45fb43084763ed8
Arthur <feasteyeson@gmail.com>
2015-09-05 13:38:17 -0700

    finished bloger

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,39 @@
 class ArticlesController < ApplicationController
+	include ArticlesHelper
 	def index
 		@articles = Article.all
 	end
 
 	def show
 		@article = Article.find_by(id: params[:id])
+	end
+
+	def new 
+		@article = Article.new
+	end
+
+	def create
+ 		@article = Article.new(article_params)
+		@article.save
+		flash.notice = "Article #{@article.title} was created"
+		redirect_to article_path(@article)
+	end
+
+	def destroy
+		article = Article.find(params[:id])
+		flash.notice = "Article #{article.title} was deleted"
+		article.destroy
+		redirect_to articles_path
+	end
+
+	def edit
+		@article = Article.find_by(id: params[:id])
+	end
+
+	def update
+		@article = Article.find_by(id: params[:id])
+		@article.update(article_params)
+		flash.notice = "Article #{@article.title} was updated!"
+		redirect_to article_path(@article)
 	end
 end
